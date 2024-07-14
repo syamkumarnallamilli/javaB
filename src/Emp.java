2617822f00f2bf828b36b498563a761937d2445c
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-14 22:28:23 +0530

    add loc to gt

---
+++ src/Emp.java
@@ -0,0 +1,13 @@
+public class Emp {
+    public static void main(String args[]){
+        String name="syam";
+        String company="dxc";
+        int salary=420000;
+        float sal=salary/12;
+        System.out.println("name:" +name);
+        System.out.println( "company:"+company);
+        System.out.println(+salary);
+        System.out.println(+sal);
+
+    }
+}
---
+++ src/Intro.java
@@ -0,0 +1,7 @@
+public class Intro {
+    public static void main(String args[]){
+        System.out.println("i am syam");
+        System.out.println("I am from andhra pradesh,mandapeta");
+    }
+    
+}
